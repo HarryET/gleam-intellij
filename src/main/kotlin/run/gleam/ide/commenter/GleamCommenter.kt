c905c6cc5b3b9c07679155a80424d2ce8b1e1b16
nadavhames <25757773+nadavhames@users.noreply.github.com>
2023-02-21 20:23:42 +0200

    feat: commenter for normal comments start

---
+++ src/main/kotlin/run/gleam/ide/commenter/GleamCommenter.kt
@@ -0,0 +1,14 @@
+package run.gleam.ide.commenter
+
+import com.intellij.lang.Commenter
+
+class GleamCommenter : Commenter {
+    override fun getLineCommentPrefixes(): MutableList<String> = mutableListOf("//", "///")
+    override fun getLineCommentPrefix(): String = "//"
+
+    override fun getBlockCommentPrefix(): String? = null
+    override fun getBlockCommentSuffix(): String? = null
+
+    override fun getCommentedBlockCommentPrefix(): String? = null
+    override fun getCommentedBlockCommentSuffix(): String? = null
+}
