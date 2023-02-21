5b48c717c15bbf19e72693992473de0827b83f56
Harry Bairstow <me@harryet.xyz>
2023-02-21 10:07:16 +0000

    chore: remove old psi components

--- src/main/kotlin/run/gleam/lang/core/lexer/GleamLexer.kt
+++
@@ -1,5 +0,0 @@
-package run.gleam.lang.core.lexer
-
-import com.intellij.lexer.FlexAdapter
-
-//class GleamLexer : FlexAdapter(GleamLexer)
--- src/main/kotlin/run/gleam/lang/core/psi/GleamTokenType.kt
+++
@@ -1,8 +0,0 @@
-package run.gleam.lang.core.psi
-
-import com.intellij.psi.tree.IElementType
-import run.gleam.lang.GleamLanguage
-
-open class GleamTokenType(debugName: String) : IElementType(debugName, GleamLanguage)
-
-open class GleamElementType(debugName: String) : IElementType(debugName, GleamLanguage)
