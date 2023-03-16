abef235d45d44616297bf76d2d02030fd56e8cbd
nadavhames <25757773+nadavhames@users.noreply.github.com>
2023-03-16 23:58:02 +0200

    Add mixin classes for type decl/alias for naming

---
+++ src/main/kotlin/run/gleam/lang/core/psi/ext/GleamTypeAlias.kt
@@ -0,0 +1,18 @@
+package run.gleam.lang.core.psi.ext
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.IStubElementType
+import run.gleam.lang.core.psi.GleamTypeAlias
+import run.gleam.lang.core.psi.GleamTypes.TYPE_NAME
+import run.gleam.lang.core.stubs.GleamTypeAliasStub
+
+abstract class GleamTypeAliasImplMixIn : GleamStubbedNamedVisibilityElementImpl<GleamTypeAliasStub>, GleamTypeAlias {
+    constructor(node: ASTNode): super(node)
+
+    constructor(stub: GleamTypeAliasStub, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
+
+    override fun getNameIdentifier(): PsiElement? {
+        return findChildByType(TYPE_NAME) ?: super.getNameIdentifier()
+    }
+}
